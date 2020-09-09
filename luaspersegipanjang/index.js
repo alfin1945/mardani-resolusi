08e0d5e160c98862de80f753e72f5f2a988db9c3
alfin1945 <69328684+alfin1945@users.noreply.github.com>
2020-09-09 20:51:49 +0700

    Add files via upload
    programmer_Pemula.

---
+++ luaspersegipanjang/index.js
@@ -0,0 +1,8 @@
+function persegi(panjang, lebar) {
+    return panjang * lebar;
+}
+
+var panjang = 3;
+var lebar = 2;
+var hasil = persegi(panjang, lebar)
+document.write("<b>diketahui:</b>" + "<br>" + "panjang : " + "<" + panjang + ">" + "<br>" + "lebar : " + "<" + lebar + ">" + "<br><br><br><br>" + "<b>jawaban:</b>" + "<br>" + "Luas persegi panjang : " + "<" + hasil + ">");
