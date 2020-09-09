4ac7ab4a4dd1cf72ec4c037388519cfdc87a2674
alfin1945 <69328684+alfin1945@users.noreply.github.com>
2020-09-09 20:55:54 +0700

    Add files via upload
    programmer_Pemula.

---
+++ pola jam pasir/index.js
@@ -0,0 +1,14 @@
+document.write("<pre>");
+
+for (let y = 1; y <= 9; y++) {
+    for (let x = 1; x <= 9; x++) {
+        if ((x <= y && x + y >= 10) || (x >= y && x + y <= 10)) {
+            document.write("*");
+        } else {
+            document.write(" ");
+        }
+    }
+    document.write("<br>");
+}
+
+document.write("</pre>");
